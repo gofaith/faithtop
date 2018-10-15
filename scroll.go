ee94df01a36bc93a427a2e3ad416fb6f7cc2ccec
StevenZack <stevenzack@qq.com>
2018-10-15 16:29:38 +0800

    Just a Backup

--- scroll.go
+++ scroll.go
@@ -32,7 +32,7 @@ func VScroll() *FScroll {
 	fb.v.SetPolicy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
 	fb.box = VBox()
 	fb.v.AddWithViewPort(fb.box.widget)
-	fb.afterShown = fb.box.afterShown
+	fb.afterShownFn = fb.box.afterShownFn
 	return fb
 }
 
@@ -41,7 +41,7 @@ func HScroll() *FScroll {
 	fb.v.SetPolicy(gtk.POLICY_AUTOMATIC, gtk.POLICY_NEVER)
 	fb.box = HBox()
 	fb.v.AddWithViewPort(fb.box.widget)
-	fb.afterShown = fb.box.afterShown
+	fb.afterShownFn = fb.box.afterShownFn
 	return fb
 }
 
