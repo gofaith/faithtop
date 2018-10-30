99b0702aabd17fcd7edfdf191e32702fa85dd1c1
StevenZack <stevenzack@qq.com>
2018-10-30 17:30:02 +0800

    add Dialog

--- window.go
+++ window.go
@@ -126,3 +126,29 @@ func (v *FWindow) Title(t string) *FWindow {
 func ShowWin(i IView) *FWindow {
 	return TopWin().Size(200, 100).Title("Infomation").DeferShow().Add(i)
 }
+func ShowInfo(w *FWindow, title, info string) {
+	dialog := gtk.NewMessageDialog(
+		w.v,
+		gtk.DIALOG_MODAL,
+		gtk.MESSAGE_INFO,
+		gtk.BUTTONS_OK,
+		info)
+	dialog.SetTitle(title)
+	dialog.Response(func() {
+		dialog.Destroy()
+	})
+	dialog.Run()
+}
+func ShowErr(w *FWindow, title, err string) {
+	dialog := gtk.NewMessageDialog(
+		w.v,
+		gtk.DIALOG_MODAL,
+		gtk.MESSAGE_ERROR,
+		gtk.BUTTONS_OK,
+		err)
+	dialog.SetTitle(title)
+	dialog.Response(func() {
+		dialog.Destroy()
+	})
+	dialog.Run()
+}
