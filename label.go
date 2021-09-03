e9ec3a1abffa42bd4e1407f03577a7ecb01bf53f
Steven Zack <zigzigcheers@gmail.com>
2021-09-03 11:20:05 +0800

    labelImage

--- label.go
+++ label.go
@@ -2,12 +2,14 @@ package faithtop
 
 type ILabel interface {
 	IWidget
+	Assign(v *ILabel) ILabel
 	/** set text of the label. e.g.,
 	Text("hello world")
 	we also support rich HTML text:
 	Text(`<html><head/><body><p>asdkhkhqw</p><p><br/></p><p><span style=" font-weight:600;">213</span></p><p><br/></p><p>12</p></body></html>``)
 	*/
 	Text(s string) ILabel
+	Image(url string, w, h int) ILabel
 	Interaction(interaction TextInteractionFlag) ILabel
 }
 
@@ -19,4 +21,8 @@ func Label() ILabel {
 
 func Label2(text string) ILabel {
 	return newLabelImpl().Text(text)
+}
+
+func LabelImage(url string, w, h int) ILabel {
+	return Label().Image(url, w, h)
 }
--- label_impl.go
+++ label_impl.go
@@ -5,6 +5,8 @@ package faithtop
 import (
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
+	"strconv"
+	"strings"
 )
 
 type LabelImpl struct {
@@ -27,7 +29,25 @@ func (l *LabelImpl) Text(s string) ILabel {
 	return l
 }
 
+func (l *LabelImpl) Assign(v *ILabel) ILabel {
+	*v = l
+	return l
+}
+
 func (l *LabelImpl) Interaction(flags TextInteractionFlag) ILabel {
 	l.label.SetTextInteractionFlags(core.Qt__TextInteractionFlag(flags))
 	return l
+}
+
+func (l *LabelImpl) Image(url string, w, h int) ILabel {
+	builder := new(strings.Builder)
+	builder.WriteString("<img src='" + url + "' ")
+	if w > 0 {
+		builder.WriteString("width='" + strconv.Itoa(w) + "' ")
+	}
+	if h > 0 {
+		builder.WriteString("height='" + strconv.Itoa(h) + "' ")
+	}
+	builder.WriteString(">")
+	return l.Text(builder.String())
 }
