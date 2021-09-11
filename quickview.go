6227d037ca710b5258b78a356596d119fe817bc6
Steven Zack <zigzigcheers@gmail.com>
2021-09-11 16:12:48 +0800

    bindFunction

--- quickview.go
+++ quickview.go
@@ -4,8 +4,10 @@ type IQuickView interface {
 	IWidget
 	Assign(v *IQuickView) IQuickView
 	Source(qmlFile string) IQuickView
-	CallQml(funcName string, args ...string)
-	RegisterFunction(funcName string, fn func(args []string)) IQuickView
+	// CallQml(funcName string, args ...string)
+	// RegisterFunction(funcName string, fn func(args []string)) IQuickView
+	BindFunction(fn interface{})IQuickView
+	Call(funcName string,args ...interface{})
 }
 
 var newQuickViewImpl func(win IWindow) IQuickView
--- quickviewimpl.go
+++ quickviewimpl.go
@@ -3,9 +3,16 @@
 package faithtop
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/quick"
 	"github.com/therecipe/qt/widgets"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 type (
@@ -20,7 +27,7 @@ type (
 		_        func()                               `constructor:"init"`
 		_        func(funcName string, args []string) `signal:"callGo,auto"`
 		_        func(funcName string, args []string) `signal:"callQml"`
-		handlers map[string]func(args []string)
+		handlers map[string]reflect.Value
 	}
 )
 
@@ -45,17 +52,55 @@ func init() {
 }
 
 func (b *Bridge) init() {
-	b.handlers = make(map[string]func(args []string))
+	b.handlers = make(map[string]reflect.Value)
 }
 func (b *Bridge) callGo(funcName string, args []string) {
 	if b.handlers == nil {
 		return
 	}
 	if handler, ok := b.handlers[funcName]; ok {
-		handler(args)
+		t := handler.Type()
+
+		if t.NumIn() > len(args) {
+			panic("not enough arguments to call Go function: [" + funcName + "], have " + strconv.Itoa(len(args)) + " but want " + strconv.Itoa(t.NumIn()))
+		}
+
+		arguments := []reflect.Value{}
+		for i := 0; i < t.NumIn(); i++ {
+			argument, e := convertToGoType(args[i], t.In(i))
+			if e != nil {
+				panic(fmt.Errorf("error when calling Go function [%s]: %w", funcName, e))
+			}
+			arguments = append(arguments, argument)
+		}
+
+		handler.Call(arguments)
+
 	} else {
 		panic("funcName [" + funcName + "] is not registered on QuickView")
 	}
+}
+
+func convertToGoType(s string, target reflect.Type) (reflect.Value, error) {
+	switch target.Kind() {
+	case reflect.String:
+		return reflect.ValueOf(s), nil
+	default:
+		value := reflect.New(target)
+		e := json.Unmarshal([]byte(s), value.Interface())
+		if e != nil {
+			return value, e
+		}
+		return value.Elem(), nil
+	}
+}
+
+func convertToQmlArgument(arg interface{}) string {
+	if v, ok := arg.(string); ok {
+		return v
+	}
+	b, _ := json.Marshal(arg)
+	return string(b)
 }
 
 func (q *QuickViewImpl) Assign(v *IQuickView) IQuickView {
@@ -68,11 +113,30 @@ func (q *QuickViewImpl) Source(qmlFile string) IQuickView {
 	return q
 }
 
-func (q *QuickViewImpl) CallQml(funcName string, args ...string) {
-	q.bridge.CallQml(funcName, args)
+func (q *QuickViewImpl) Call(funcName string, args ...interface{}) {
+	strs := []string{}
+	for _, arg := range args {
+		strs = append(strs, convertToQmlArgument(arg))
+	}
+	q.bridge.CallQml(funcName, strs)
 }
 
-func (q *QuickViewImpl) RegisterFunction(funcName string, fn func(args []string)) IQuickView {
-	q.bridge.handlers[funcName] = fn
+func (q *QuickViewImpl) BindFunction(fn interface{}) IQuickView {
+	funcName := getFunctionName(fn)
+	t := reflect.TypeOf(fn)
+	if t.Kind() != reflect.Func {
+		panic("BindFunction: the input value is not a function, but [" + t.Kind().String() + "]")
+	}
+	if _, ok := q.bridge.handlers[funcName]; ok {
+		panic("BindFunction: function with '" + funcName + "' duplicated")
+	}
+	q.bridge.handlers[funcName] = reflect.ValueOf(fn)
 	return q
+}
+
+func getFunctionName(v interface{}) string {
+	t := reflect.ValueOf(v)
+	fn := runtime.FuncForPC(t.Pointer()).Name()
+	n := strings.TrimPrefix(filepath.Ext(fn), ".")
+	return n
 }
